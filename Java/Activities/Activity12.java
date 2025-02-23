17b5beb5bc3db0093ae006e5931e53739bda068a
Mahantesh-lab <58810244+Mahantesh-lab@users.noreply.github.com>
2025-02-23 16:32:48 +0530

    Update Activity12.java

--- Java/Activities/Activity12.java
+++ Java/Activities/Activity12.java
@@ -3,7 +3,7 @@ package fst_m1_java;
 public class Activity12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Mahantesh Assignment");
 		AddableActivity12 a1 = (num1, num2) -> (num1+num2);
 		System.out.println(a1.add(10,20));
 		
